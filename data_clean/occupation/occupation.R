5306c00a2cda370f180e1f3732d8542fb842adfb
petertanksley <54998131+petertanksley@users.noreply.github.com>
2024-05-28 10:35:14 -0500

    Added occupation dir

---
+++ data_clean/occupation/occupation.R
@@ -0,0 +1,25 @@
+source("../cleaning_packages.R")
+
+
+
+
+
+
+
+
+
+
+
+#=====update log file==========================================================
+
+#write update message
+message="
+Initialized log file.
+"
+
+#update log
+update_log(file="log_occupation.txt",
+           author="Peter T. Tanksley",
+           message = message)
+
+
