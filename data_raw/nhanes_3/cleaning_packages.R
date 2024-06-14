774283b4db8666d88e4931614b1f7f6431864e27
petertanksley <54998131+petertanksley@users.noreply.github.com>
2024-06-14 14:10:01 -0500

    Added and completed exam data setup

--- data_raw/nhanes_3/cleaning_packages.R
+++ data_raw/nhanes_3/cleaning_packages.R
@@ -36,6 +36,46 @@ read_lines_to_df <- function(file_path, start_line, end_line) {
   return(df)
 }
 
+#function for creating/updating a log file for each directory ()
+update_log <- function(file, author, message) {
+  # Get the current date
+  sysdate <- format(Sys.Date(), "%m-%d-%Y")
+  # Wrap the message text
+  wrapped_message <- paste(strwrap(message, width = 80), collapse = "\n")
+  
+  # Check if the file exists
+  if (file.exists(file)) {
+    # Read existing contents of the file
+    existing_content <- readLines(file)
+    
+    # Append new content to the top
+    new_content <- paste(sysdate, author, "",  "Message:", wrapped_message, "","", sep = "\n")
+    all_content <- c(new_content, existing_content)
+    
+    # Write all content back to the file
+    writeLines(all_content, file)
+  } else {
+    # If file doesn't exist, create a new one
+    write(paste(sysdate, author, "",  "Message:", wrapped_message, "","", sep = "\n"), file)
+  }
+}
+
+
+#=====Exit message=============================================================
+cat("Make sure to update the log file using {update_log()} after you're done! \n \n")
+cat('EXAMPLE:
+
+#=====update log file==========================================================
+
+#write update message
+message="
+(list of changes made)
+"
+
+#update log
+update_log(file="log_[filename].txt",
+           author="[name]",
+           message = message)')
 
 
 
