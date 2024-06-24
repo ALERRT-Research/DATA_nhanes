5f7bb2e0f942822ea45d50f8ceea5238d2e91f60
petertanksley <54998131+petertanksley@users.noreply.github.com>
2024-06-24 11:51:11 -0500

    Added checksum procedure for NHC mortality data.

--- data_raw/NHANES_C/mortality/mortality.R
+++ data_raw/NHANES_C/mortality/mortality.R
@@ -1,22 +1,33 @@
 source("../cleaning_packages.R")
 
-# batch download
-start_yr <- seq(1999, 2017, 2)
-map(start_yr, get_nhanes_mort) 
+#check checksum
+checksum_result <- check_md5_file("mortality_raw.rds")
 
-mort_files <- list.files(pattern = "NHANES")
-mort_raw <- map(mort_files, read_nhanes_mort) |> 
-  list_rbind()
 
-export(mort_raw, "mortality_raw.rds")
-
+if(!file.exists("mortality_raw.rds") || !checksum_result){
+  
+  # batch download
+  start_yr <- seq(1999, 2017, 2)
+  map(start_yr, get_nhanes_mort) 
+  
+  #read in files and bind rows
+  mort_files <- list.files(pattern = "NHANES")
+  mort_raw <- map(mort_files, read_nhanes_mort) |> 
+    list_rbind()
+  
+  #export data
+  export(mort_raw, "mortality_raw.rds")
+  
+  #create checksum file
+  create_md5_file("mortality_raw.rds")
+}
 
 
 #=====update log file==========================================================
 
 #write update message
 message="
-Initiate log.
+Added checksum procedure for NHC mortality data.
 "
 
 #update log
