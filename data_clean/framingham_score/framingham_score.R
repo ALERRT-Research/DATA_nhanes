638ca64b96d2fcf010a7fceee1b50a9c2e64ae1a
petertanksley <54998131+petertanksley@users.noreply.github.com>
2024-08-01 09:49:06 -0500

    Added Framingham score file

---
+++ data_clean/framingham_score/framingham_score.R
@@ -0,0 +1,28 @@
+source("../cleaning_packages.R")
+
+#load package to compute different CV risk scores
+p_load(CVrisk)
+
+#=====Read in cleaned data=====================================================
+
+
+
+#=====Framingham 10yr ASCVD score (lab measurements)===========================
+
+ascvd_10y_frs()
+
+# gender = c("male", "female"), 
+# age, 
+# hdl, 
+# totchol, 
+# sbp, 
+# bp_med, 
+# smoker, 
+# diabetes,
+
+
+
+
+
+
+
