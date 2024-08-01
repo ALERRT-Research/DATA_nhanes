fdb5e3cb7e56ebd0f136193a99d7e42d908232f9
petertanksley <54998131+petertanksley@users.noreply.github.com>
2024-08-01 09:47:08 -0500

    Update demographics_download.R

--- data_raw/NHANES_C/demographics/demographics_download.R
+++ data_raw/NHANES_C/demographics/demographics_download.R
@@ -1,5 +1,8 @@
 source("../cleaning_packages.R")
 
+#get codebook for all questionnaire data files
+quest_codebook <- nhanes_table("https://wwwn.cdc.gov/nchs/nhanes/search/variablelist.aspx?Component=Questionnaire&Cycle=")
+
 #check checksums
 checksum_result <- check_md5_file("demographics_raw.rds")
 
@@ -7,10 +10,11 @@ checksum_result <- check_md5_file("demographics_raw.rds")
 if (!file.exists("demographics_raw.rds") || !checksum_result) {
   
   # Get waves (1999-2017)
-  names_demo <- c("DEMO", paste0("DEMO_", LETTERS[2:10]))
+  names_demo <- c("SMQ", paste0("SMQ_", LETTERS[2:10]))
   years_demo <- seq(1999, 2017, by=2)
   
-  demo <- pull_nhanes(names_demo, years_demo, mismatch_regex = "DMD..SIZ|DMDHHSZ.")
+  smok <- pull_nhanes(names_demo, years_demo, mismatch_regex = "SMD100LN|SMQ666K|SMQ665D"
+                      )
   
   #export data
   export(demo, "demographics_raw.rds")
